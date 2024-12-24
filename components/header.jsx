f55e48c349018afda1b3d1c81ecbedf2b5efa6db
ShehrozKashif <sharooz57@gmail.com>
2024-12-24 23:31:26 +0500

    completed authorization through clerk

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,22 @@
 // Header Component (header.js)
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 const Header = () => {
-    return <div>Header</div>;
-  };
-  
-  export default Header;
-  
+  return (
+    <header className="bg-gray-100 py-4 px-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">CapitalCore</h1>
+        <div>
+          <SignedOut>
+            <SignInButton mode="modal" />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
