4e02f69a6796478a8ddd253f857d405a643e8c26
ShehrozKashif <sharooz57@gmail.com>
2024-12-24 22:37:01 +0500

    added header and footer and configured clerk for authorization

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,40 @@
+// RootLayout Component (layout.js)
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-const inter = Inter({subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "capitalcore",
-  description: "fianance managing platform",
+  description: "finance managing platform",
 };
 
 export default function RootLayout({ children }) {
   return (
+    
+
+    <ClerkProvider>
+
+
     <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        {children}
+      <body className={inter.className}>
+        {/* Header */}
+        <Header />
+        
+        {/* Main Content */}
+        <main className="min-h-screen">{children}</main>
+        
+        {/* Footer */}
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>&copy; 2024 Shehroz Kashif. All rights reserved.</p>
+          </div>
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
+
   );
 }
---
+++ components/header.jsx
@@ -0,0 +1,7 @@
+// Header Component (header.js)
+const Header = () => {
+    return <div>Header</div>;
+  };
+  
+  export default Header;
+  
